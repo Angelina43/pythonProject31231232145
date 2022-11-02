afac0ab1ebd811c67f52a8332b881e4c072736aa
gr413_buayur <bushueva.03@inbox.ru>
2022-11-02 11:52:57 +0700

    sdelana rabota

--- bboard/main/urls.py
+++ bboard/main/urls.py
@@ -2,27 +2,33 @@ from django.urls import path
 from .views import index
 from .views import other_page
 from .views import BBLoginView
+from .views import profile_bb_add
 from .views import profile
 from .views import BBLogoutView
 from .views import ChangeUserInfoView
 from .views import BBPasswordChangeView
 from .views import RegisterUserView, RegisterDoneView
-from .views import DeleteUserView
 from .views import user_activate
+from .views import DeleteUserView
 from .views import by_rubric
 from .views import detail
-from .views import profile_bb_add
 from .views import profile_bb_delete
 from .views import profile_bb_change
+
 
 app_name = 'main'
 
 urlpatterns = [
-   path('accounts/logout/', BBLogoutView.as_view(), name='logout'),
-   path('accounts/profile/', profile, name='profile'),
-   path('accounts/login', BBLoginView.as_view(), name='login'),
+   path('<int:rubric_pk>/<int:pk>/', detail, name='detail'),
+   path('<int:pk>/', by_rubric, name='by_rubric'),
    path('<str:page>/', other_page, name='other'),
    path('', index, name='index'),
+   path('accounts/login', BBLoginView.as_view(), name='login'),
+   path('accounts/profile/add/', profile_bb_add, name='profile_bb_add'),
+   path('accounts/profile/change/<int:pk>/', profile_bb_change, name='profile_bb_change'),
+   path('accounts/profile/delete/<int:pk>/', profile_bb_delete, name='profile_bb_delete'),
+   path('accounts/profile/', profile, name='profile'),
+   path('accounts/logout/', BBLogoutView.as_view(), name='logout'),
    path('accounts/profile/change/', ChangeUserInfoView.as_view(), name='profile_change'),
    path('accounts/profile/', profile, name='profile'),
    path('accounts/password/change/', BBPasswordChangeView.as_view(), name='password_change'),
@@ -33,13 +39,4 @@ urlpatterns = [
    path('accounts/register/done/', RegisterDoneView.as_view(), name='register_done'),
    path('accounts/profile/delete/', DeleteUserView.as_view(), name='profile_delete'),
    path('accounts/profile/change/', ChangeUserInfoView.as_view(), name='profile_change'),
-   path('<int:pk>/', by_rubric, name='by_rubric'),
-   path('<str:page>/', other_page, name='other'),
-   path('<int:rubric_pk>/<int:pk>/', detail, name='detail'),
-   path('<int:pk>/', by_rubric, name='by_rubric'),
-   path('accounts/profile/add/', profile_bb_add, name='profile_bb_add'),
-   path('accounts/profile/', profile, name='profile'),
-   path('accounts/profile/change/<int:pk>/', profile_bb_change, name='profile_bb_change'),
-   path('accounts/profile/delete/<int:pk>/', profile_bb_delete, name='profile_bb_delete'),
-   path('accounts/profile/', profile, name='profile'),
 ]
